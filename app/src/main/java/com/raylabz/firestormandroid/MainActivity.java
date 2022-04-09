043945a39ada71ced9313e08151d82fef48606fe
nkasenides <hfnovember@hotmail.com>
2022-04-09 17:42:01 +0300

    Fixes for failure of tasks

--- app/src/main/java/com/raylabz/firestormandroid/MainActivity.java
+++ app/src/main/java/com/raylabz/firestormandroid/MainActivity.java
@@ -6,6 +6,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
@@ -18,6 +19,7 @@ public class MainActivity extends AppCompatActivity {
     Button getButton;
     Button deleteButton;
     Button getManyButton;
+    Button existsButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,12 +30,14 @@ public class MainActivity extends AppCompatActivity {
         getButton = findViewById(R.id.buttonGet);
         deleteButton = findViewById(R.id.buttonDelete);
         getManyButton = findViewById(R.id.buttonGetMany);
+        existsButton = findViewById(R.id.buttonExists);
 
         createButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Person p = new Person("aaa", "Nicos", 29);
                 Firestorm.create(p, p.getId());
+                Toast.makeText(MainActivity.this, p.getId(), Toast.LENGTH_SHORT).show();
             }
         });
 
@@ -44,7 +48,13 @@ public class MainActivity extends AppCompatActivity {
                         .addOnSuccessListener(new OnSuccessListener<Person>() {
                             @Override
                             public void onSuccess(Person person) {
-                                System.out.println(person);
+                                Toast.makeText(MainActivity.this, person.toString(), Toast.LENGTH_SHORT).show();
+                            }
+                        })
+                        .addOnFailureListener(new OnFailureListener() {
+                            @Override
+                            public void onFailure(@NonNull Exception e) {
+                                Toast.makeText(MainActivity.this, "Failed to get object", Toast.LENGTH_SHORT).show();
                             }
                         });
             }
@@ -64,7 +74,20 @@ public class MainActivity extends AppCompatActivity {
                         .addOnSuccessListener(new OnSuccessListener<List<Person>>() {
                             @Override
                             public void onSuccess(List<Person> people) {
-                                System.out.println(people);
+                                Toast.makeText(MainActivity.this, people.toString(), Toast.LENGTH_SHORT).show();
+                            }
+                        });
+            }
+        });
+
+        existsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Firestorm.exists(Person.class, "aaa")
+                        .addOnSuccessListener(new OnSuccessListener<Boolean>() {
+                            @Override
+                            public void onSuccess(Boolean aBoolean) {
+                                Toast.makeText(MainActivity.this, aBoolean.toString(), Toast.LENGTH_SHORT).show();
                             }
                         });
             }
--- firestorm-android/src/main/java/com/raylabz/firestormandroid/Firestorm.java
+++ firestorm-android/src/main/java/com/raylabz/firestormandroid/Firestorm.java
@@ -125,6 +125,14 @@ public final class Firestorm {
                     if (task.isSuccessful()) {
                         source.setResult(reference.getId());
                     }
+                    else {
+                        if (task.getException() != null) {
+                            source.setException(task.getException());
+                        }
+                        else {
+                            source.setException(new FirestormObjectException("Could not write object with ID '" + reference.getId() + "'."));
+                        }
+                    }
                 });
             });
             return source.getTask();
@@ -153,6 +161,14 @@ public final class Firestorm {
                     if (task.isSuccessful()) {
                         source.setResult(reference.getId());
                     }
+                    else {
+                        if (task.getException() != null) {
+                            source.setException(task.getException());
+                        }
+                        else {
+                            source.setException(new FirestormObjectException("Could not write object with ID '" + id + "'."));
+                        }
+                    }
                 });
             });
             return source.getTask();
@@ -177,7 +193,12 @@ public final class Firestorm {
             docRef.get().addOnCompleteListener(task -> {
                 if (task.isSuccessful()) {
                     T object = task.getResult().toObject(objectClass);
-                    source.setResult(object);
+                    if (object == null) {
+                        source.setException(new FirestormObjectException("Object with ID '" + documentID + "' not found."));
+                    }
+                    else {
+                        source.setResult(object);
+                    }
                 }
             });
         });
@@ -220,79 +241,36 @@ public final class Firestorm {
         ArrayList<String> idsList = new ArrayList<>(Arrays.asList(ids));
         return getMany(objectClass, idsList);
     }
-//
-//    /**
-//     * Retrieves multiple documents of a class as a list of objects.
-//     * @param objectClass The class of the objects.
-//     * @param ids A list of IDs of the objects to retrieve.
-//     * @param <T> A type matching the type of object class.
-//     * @param onFailureListener FailureListener to execute onFailure().
-//     * @return Returns a list of type T.
-//     */
-//    public static <T> List<T> getMany(final Class<T> objectClass, List<String> ids, OnFailureListener onFailureListener) {
-//        ArrayList<T> retItems = new ArrayList<>();
-//        final DocumentReference[] documentReferences = new DocumentReference[ids.size()];
-//        for (int i = 0; i < documentReferences.length; i++) {
-//            documentReferences[i] = (firestore.collection(objectClass.getSimpleName()).document(ids.get(i)));
-//        }
-//        final ApiFuture<List<DocumentSnapshot>> items = firestore.getAll(documentReferences);
-//        try {
-//            final List<DocumentSnapshot> documentSnapshots = items.get();
-//            for (DocumentSnapshot documentSnapshot : documentSnapshots) {
-//                if (documentSnapshot.exists()) {
-//                    retItems.add(documentSnapshot.toObject(objectClass));
-//                }
-//            }
-//            return retItems;
-//        } catch (ExecutionException | InterruptedException e) {
-//            onFailureListener.onFailure(e);
-//            return null;
-//        }
-//    }
-//
-//    /**
-//     * Retrieves a document as an object from Firestore.
-//     *
-//     * @param objectClass The class of the object retrieved.
-//     * @param documentID  The documentID of the object to retrieve.
-//     * @param <T>         A type matching the type of objectClass.
-//     * @return Returns an object of type T (objectClass).
-//     * @throws FirestormException Thrown when Firestorm encounters an error.
-//     */
-//    public static <T> T get(final Class<T> objectClass, final String documentID) throws FirestormException {
-//        DocumentReference docRef = firestore.collection(objectClass.getSimpleName()).document(documentID);
-//        ApiFuture<DocumentSnapshot> future = docRef.get();
-//        try {
-//            DocumentSnapshot document = future.get();
-//            if (document.exists()) {
-//                return document.toObject(objectClass);
-//            }
-//            else {
-//                return null;
-//            }
-//        } catch (InterruptedException | ExecutionException | NotInitializedException e) {
-//            throw new FirestormException(e);
-//        }
-//    }
-//
-//    /**
-//     * Checks if a given documentID of a given class exists.
-//     *
-//     * @param objectClass The object class.
-//     * @param documentID The document ID.
-//     * @return Returns true if the document exists on Firestore, false otherwise.
-//     * @throws FirestormException Thrown when Firestorm encounters an error.
-//     */
-//    public static boolean exists(final Class<?> objectClass, final String documentID) throws FirestormException {
-//        DocumentReference docRef = firestore.collection(objectClass.getSimpleName()).document(documentID);
-//        ApiFuture<DocumentSnapshot> future = docRef.get();
-//        try {
-//            DocumentSnapshot document = future.get();
-//            return document.exists();
-//        } catch (InterruptedException | ExecutionException | NotInitializedException e) {
-//            throw new FirestormException(e);
-//        }
-//    }
+
+    /**
+     * Checks if a given documentID of a given class exists.
+     *
+     * @param objectClass The object class.
+     * @param documentID The document ID.
+     * @return Returns true if the document exists on Firestore, false otherwise.
+     * @throws FirestormException Thrown when Firestorm encounters an error.
+     */
+    public static Task<Boolean> exists(final Class<?> objectClass, final String documentID) throws FirestormException {
+        DocumentReference docRef = firestore.collection(objectClass.getSimpleName()).document(documentID);
+        TaskCompletionSource<Boolean> source = new TaskCompletionSource<>();
+        new Handler().post(() -> {
+            docRef.get().addOnCompleteListener(task -> {
+                if (task.isSuccessful()) {
+                    boolean exists = task.getResult().exists();
+                    source.setResult(exists);
+                }
+                else {
+                    if (task.getException() != null) {
+                        source.setException(task.getException());
+                    }
+                    else {
+                        source.setException(new FirestormObjectException("Could not check object with ID '" + documentID + "'."));
+                    }
+                }
+            });
+        });
+        return source.getTask();
+    }
 //
 //    /**
 //     * Checks if a given documentID of a given class exists.
